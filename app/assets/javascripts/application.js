6d9bdf96fe9b15580be0e15528b9edb5a488afb5
Douglas Uretsky <douglasuretsky@gmail.com>
2019-03-11 14:18:52 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require wrong_place_wrong_time
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
